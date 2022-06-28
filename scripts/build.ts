b74da596df3aaea9cb960e5919413fa7cec0e746
jakehl <jake@twilsoft.uk>
2022-06-28 22:51:07 +0100

    fixed build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -5,7 +5,11 @@ import { tryifyAsync as ta } from "tryify";
 const runBuild = async () => {
   const outdir = "./build/";
 
-  await ta(Deno.remove)(outdir);
+  const [error] = await ta(Deno.remove)(outdir, { recursive: true });
+  if (error) {
+    console.log(error);
+    Deno.exit(1);
+  }
   await fs.copy("./static/", outdir);
 
   const entryPoints = ["./src/background.ts", "./src/content.ts"];
